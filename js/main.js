a72105794a0c686dcd6f7c7b4caac08dec97faa1
diego.data.icloud@gmail.com <diego.data.icloud@gmail.com>
2022-05-13 14:06:21 -0300

    agregue array de clientas

--- js/main.js
+++ js/main.js
@@ -44,3 +44,24 @@ const turno1 = new turno(
 turno1.hablar();
 alert("Llama por telefono al 555-6666 para reconfirmar");
 alert("Gracias!");
+
+class ClientaTurnos {
+  constructor(nombre, telefono, servicio) {
+    this.nombre = nombre;
+    this.telefono = telefono;
+    this.servicio = servicio;
+  }
+}
+const clientas = [];
+clientas.push(
+  new ClientaTurnos(
+    prompt("Ingresa nombre"),
+    prompt("Ingresa telefono: "),
+    "Depilacion"
+  )
+);
+clientas.push(new ClientaTurnos("Maria", "4555 - 1855", "Belleza de pies"));
+clientas.push(new ClientaTurnos("Carolina", "3412 - 4458", "Depilacion"));
+clientas.push(new ClientaTurnos("Elena", "38546 - 1122", "Cosmetologia"));
+clientas.push(new ClientaTurnos("Belen", "4784 - 4584", "Manos"));
+console.log(clientas);
